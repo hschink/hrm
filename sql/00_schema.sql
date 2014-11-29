f2919c85698f387b70b24b6280dcb80a64ef52bc
Hagen Schink <hagen.schink@gmail.com>
2014-11-29 23:17:41 +0100

    Rename table customers column surname to lastname

--- sql/00_schema.sql
+++ sql/00_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customers(
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	firstname varchar(255),
-	surname varchar(255),
+	lastname varchar(255),
 	address varchar(255),
 	telephone varchar(255),
 	taken_care_of_by integer references salespersons(id)
